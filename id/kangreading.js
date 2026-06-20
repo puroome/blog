4b4e152e2caca6d59192361a64bd1028ee3b72ab
puroome <129413484+puroome@users.noreply.github.com>
2026-06-21 00:04:28 +0900

    Add files via upload

---
+++ id/kangreading.js
@@ -0,0 +1,35 @@
+window.blogsData = window.blogsData || [];
+(function() {
+    let blog = {
+        "id": "kangreading",
+        "name": "kangreading",
+        "blogUrl": "https://blog.naver.com/kangreading",
+        "categoryNo": "89",
+        "parentCategoryNo": "",
+        "posts": []
+    };
+    blog.posts.push({"id": "post_224308353326", "title": "[독해 패턴구조] 1. 문장 맨 앞 That절과 What절은 주어", "link": "https://blog.naver.com/kangreading/224308353326"});
+    blog.posts.push({"id": "post_223032389239", "title": "[강쌤GRE] 2012.9월 ~ 2022.3월 GRE Issue 기출문제 빈도별 정리", "link": "https://blog.naver.com/kangreading/223032389239"});
+    blog.posts.push({"id": "post_223016610739", "title": "[강쌤GRE] GRE Verbal 160점~164점 및 165점 이상 전략", "link": "https://blog.naver.com/kangreading/223016610739"});
+    blog.posts.push({"id": "post_224298722757", "title": "[독해 구조패턴] 영어 단어는 다 아는데 해석이 엉키는 이유", "link": "https://blog.naver.com/kangreading/224298722757"});
+    blog.posts.push({"id": "post_224299661261", "title": "[독해 구조패턴] “That all you need is love is clear” — 상위 1%만 아는 구조", "link": "https://blog.naver.com/kangreading/224299661261"});
+    blog.posts.push({"id": "post_224302834887", "title": "[독해 구조패턴] 30년 강의 끝에 내린 결론 — 영어 실력은 '보는 순서'가 결정한다", "link": "https://blog.naver.com/kangreading/224302834887"});
+    blog.posts.push({"id": "post_224304499097", "title": "[독해 구조패턴] 콤마(,) 하나로 영문 해석이 달라진다", "link": "https://blog.naver.com/kangreading/224304499097"});
+    blog.posts.push({"id": "post_223013862938", "title": "[강쌤GRE] 2011.8월~2021.2월 GRE 후기단어 모음집 <무료배포>", "link": "https://blog.naver.com/kangreading/223013862938"});
+    blog.posts.push({"id": "post_224318499200", "title": "[독해 패턴구조] 6. 문장 맨 앞 All, Some, Many, Much, Few, Little은주어이거나 주어 일부", "link": "https://blog.naver.com/kangreading/224318499200"});
+    blog.posts.push({"id": "post_224310205189", "title": "[독해 패턴구조] 2. 문장 맨 앞 Whether절, Where절, When절은주어이거나 부사어", "link": "https://blog.naver.com/kangreading/224310205189"});
+    blog.posts.push({"id": "post_224312415508", "title": "[독해 패턴구조] 3. 문장 맨 앞 Why절과 How절은 주어", "link": "https://blog.naver.com/kangreading/224312415508"});
+    blog.posts.push({"id": "post_224316396980", "title": "[독해 패턴구조] 5. 문장 맨 앞 It은 5가지 유형의 주어", "link": "https://blog.naver.com/kangreading/224316396980"});
+    blog.posts.push({"id": "post_224314730231", "title": "[독해 패턴구조] 4. 문장 맨 앞 'To + 동사원형'과 '-ing'는주어이거나 부사어", "link": "https://blog.naver.com/kangreading/224314730231"});
+    blog.posts.push({"id": "post_223306107554", "title": "[독해 문장구조] 패턴3-1. 문장 맨 앞에 있는 All, Any, Both, Each, Some 등은 주어이거나 주어의 일부이다", "link": "https://blog.naver.com/kangreading/223306107554"});
+    blog.posts.push({"id": "post_223302221078", "title": "[독해 문장구조] 패턴2-2. 문장 맨 앞에 있는 It은 인칭대명사, 비인칭주어, 상황의 it, 강조구문, 가주어 중 하나", "link": "https://blog.naver.com/kangreading/223302221078"});
+    blog.posts.push({"id": "post_223310629010", "title": "[독해 문장구조] 패턴3-2. 문장 맨 앞에 있는 Many, Much, Few, Little, Several은 주어이거나 주어의 일부", "link": "https://blog.naver.com/kangreading/223310629010"});
+    blog.posts.push({"id": "post_223332738349", "title": "[독해 문장구조] 패턴4-4. 문장 맨 앞에 있는 Where절은 주어 또는 부사어이다", "link": "https://blog.naver.com/kangreading/223332738349"});
+    blog.posts.push({"id": "post_223298141880", "title": "[독해 문장구조] 패턴1-2. 문장 맨 앞에 있는 명사어구는 주어", "link": "https://blog.naver.com/kangreading/223298141880"});
+    blog.posts.push({"id": "post_223323346607", "title": "[독해 문장구조] 패턴4-3. 문장 맨 앞에 있는 Whether절은 주어 또는 부사어이다", "link": "https://blog.naver.com/kangreading/223323346607"});
+    blog.posts.push({"id": "post_223318615009", "title": "[독해 문장구조] 패턴4-2. 평서문에서 문장 맨 앞에 있는 What은 주어의 일부이다", "link": "https://blog.naver.com/kangreading/223318615009"});
+    blog.posts.push({"id": "post_223297857481", "title": "[독해 문장구조] 패턴1-1. 문장 맨 앞에 있는 명사어구는 주어", "link": "https://blog.naver.com/kangreading/223297857481"});
+    blog.posts.push({"id": "post_223316511935", "title": "[독해 문장구조] 패턴4-1. 문장 맨 앞에 있는 That은 주어이거나 주어의 일부이다", "link": "https://blog.naver.com/kangreading/223316511935"});
+    blog.posts.push({"id": "post_223300784976", "title": "[독해 문장구조] 패턴2-1. 문장 맨 앞에 있는 It은 인칭대명사, 비인칭주어, 상황의 it, 강조구문, 가주어 중 하나", "link": "https://blog.naver.com/kangreading/223300784976"});
+    window.blogsData.push(blog);
+})();
---
+++ id/kangreading_1.js
@@ -0,0 +1,78 @@
+window.blogsData = window.blogsData || [];
+(function() {
+    let blog = {
+        "id": "kangreading_1",
+        "name": "kangreading_1",
+        "blogUrl": "https://blog.naver.com/kangreading",
+        "categoryNo": "38",
+        "parentCategoryNo": "",
+        "posts": []
+    };
+    blog.posts.push({"id": "post_223305088910", "title": "[강쌤 문장구조] 대쉬(—) 용법 총정리", "link": "https://blog.naver.com/kangreading/223305088910"});
+    blog.posts.push({"id": "post_223302975776", "title": "[강쌤 문장구조] 콤마(쉼표; comma) 용법 총정리", "link": "https://blog.naver.com/kangreading/223302975776"});
+    blog.posts.push({"id": "post_224243417347", "title": "[강쌤 문장구조] 10명 중 8명이 틀리는 이 문장, 왜...?", "link": "https://blog.naver.com/kangreading/224243417347"});
+    blog.posts.push({"id": "post_223013862938", "title": "[강쌤GRE] 2011.8월~2021.2월 GRE 후기단어 모음집 <무료배포>", "link": "https://blog.naver.com/kangreading/223013862938"});
+    blog.posts.push({"id": "post_224244704682", "title": "[강쌤 문장구조] 단어는 다 아는데 왜 해석이 안 될까?", "link": "https://blog.naver.com/kangreading/224244704682"});
+    blog.posts.push({"id": "post_223298616935", "title": "[강쌤 문장구조] Ⅶ.3. 부정 부사가 문장 맨 앞에 있을 때 어순 도치(Inversion)", "link": "https://blog.naver.com/kangreading/223298616935"});
+    blog.posts.push({"id": "post_224242930384", "title": "[출간 안내] 《한 눈에 보이는 영어 문장구조》 출간되었습니다", "link": "https://blog.naver.com/kangreading/224242930384"});
+    blog.posts.push({"id": "post_223299616392", "title": "[강쌤 문장구조] Ⅶ.4. 접속사 as 뒤 절에서 생략과 도치", "link": "https://blog.naver.com/kangreading/223299616392"});
+    blog.posts.push({"id": "post_223272134271", "title": "[강쌤 문장구조] Ⅲ-3. 접속사 for와 in that이 이끄는 부사절", "link": "https://blog.naver.com/kangreading/223272134271"});
+    blog.posts.push({"id": "post_224247453935", "title": "[강쌤 문장구조] 영어 5형식의 한계 — 긴 문장이 갑자기 어려워지는 진짜 이유", "link": "https://blog.naver.com/kangreading/224247453935"});
+    blog.posts.push({"id": "post_223016610739", "title": "[강쌤GRE] GRE Verbal 160점~164점 및 165점 이상 전략", "link": "https://blog.naver.com/kangreading/223016610739"});
+    blog.posts.push({"id": "post_223032389239", "title": "[강쌤GRE] 2012.9월 ~ 2022.3월 GRE Issue 기출문제 빈도별 정리", "link": "https://blog.naver.com/kangreading/223032389239"});
+    blog.posts.push({"id": "post_223303962273", "title": "[강쌤 문장구조] 콜론(", "link": "https://blog.naver.com/kangreading/223303962273"});
+    blog.posts.push({"id": "post_223300894885", "title": "[강쌤 문장구조] Ⅶ.5. 비교절에서 어순도치", "link": "https://blog.naver.com/kangreading/223300894885"});
+    blog.posts.push({"id": "post_223295408708", "title": "[강쌤 문장구조] Ⅶ.1. 어순 도치(Inversion), 강조어구나 긴 어구는 뒤에 배치", "link": "https://blog.naver.com/kangreading/223295408708"});
+    blog.posts.push({"id": "post_223262668875", "title": "[강쌤 문장구조] Ⅱ.8.1. 부정사구와 분사구 차이 ... 총정리", "link": "https://blog.naver.com/kangreading/223262668875"});
+    blog.posts.push({"id": "post_223287930507", "title": "[강쌤 문장구조] Ⅴ. 병렬구조; and, or, but 용법", "link": "https://blog.naver.com/kangreading/223287930507"});
+    blog.posts.push({"id": "post_223252305856", "title": "[강쌤 문장구조] Ⅰ.5.1. 부정사구와 동명사구 차이 ... 총정리", "link": "https://blog.naver.com/kangreading/223252305856"});
+    blog.posts.push({"id": "post_223291859048", "title": "[강쌤 문장구조] Ⅵ.2. 비교급 비교의 구조", "link": "https://blog.naver.com/kangreading/223291859048"});
+    blog.posts.push({"id": "post_223292822791", "title": "[강쌤 문장구조] Ⅵ.3. 이중 비교의 구조", "link": "https://blog.naver.com/kangreading/223292822791"});
+    blog.posts.push({"id": "post_223285438903", "title": "[강쌤 문장구조] 관계사의 모든 것 2, 관계부사와 복합 관계부사", "link": "https://blog.naver.com/kangreading/223285438903"});
+    blog.posts.push({"id": "post_223288975121", "title": "[강쌤 문장구조] Ⅵ. 비교구문의 구조", "link": "https://blog.naver.com/kangreading/223288975121"});
+    blog.posts.push({"id": "post_223290659301", "title": "[강쌤 문장구조] Ⅵ.1. 원급 비교의 구조", "link": "https://blog.naver.com/kangreading/223290659301"});
+    blog.posts.push({"id": "post_223296435033", "title": "[강쌤 문장구조] Ⅶ.2. 자동사 구문에서 어순 도치(Inversion)", "link": "https://blog.naver.com/kangreading/223296435033"});
+    blog.posts.push({"id": "post_223274020073", "title": "[강쌤 문장구조] Ⅲ-5. 부정사구와 분사구문의 차이점 ... 총정리", "link": "https://blog.naver.com/kangreading/223274020073"});
+    blog.posts.push({"id": "post_223286501867", "title": "[강쌤 문장구조] 준동사(부정사, 동명사, 분사) ... 총정리", "link": "https://blog.naver.com/kangreading/223286501867"});
+    blog.posts.push({"id": "post_223293981624", "title": "[강쌤 문장구조] Ⅵ.4. 최상급 비교의 구조", "link": "https://blog.naver.com/kangreading/223293981624"});
+    blog.posts.push({"id": "post_223277992088", "title": "[강쌤 문장구조] Ⅲ-9. <With + 명사 + 현재분사/과거분사/형용사>, 부대상황이라 불리는 이유", "link": "https://blog.naver.com/kangreading/223277992088"});
+    blog.posts.push({"id": "post_223264975365", "title": "[강쌤 문장구조] Ⅱ.10. 전치사구가 명사를 수식하는 형용사구 용법", "link": "https://blog.naver.com/kangreading/223264975365"});
+    blog.posts.push({"id": "post_223274497014", "title": "[강쌤 문장구조] Ⅲ-6. <too ~ to부정사>와 <enough ~ to부정사> 용법", "link": "https://blog.naver.com/kangreading/223274497014"});
+    blog.posts.push({"id": "post_223250315383", "title": "[강쌤 문장구조] It is ~ that ~", "link": "https://blog.naver.com/kangreading/223250315383"});
+    blog.posts.push({"id": "post_223273088198", "title": "[강쌤 문장구조] Ⅲ-4. 종속접속사와 접속부사의 차이점 ... 바로 ‘이것’", "link": "https://blog.naver.com/kangreading/223273088198"});
+    blog.posts.push({"id": "post_223282014715", "title": "[강쌤 문장구조] That에 관한 모든 것, 완전 정복편", "link": "https://blog.naver.com/kangreading/223282014715"});
+    blog.posts.push({"id": "post_223283948516", "title": "[강쌤 문장구조] 관계사의 모든 것 1, 관계대명사와 복합 관계대명사", "link": "https://blog.naver.com/kangreading/223283948516"});
+    blog.posts.push({"id": "post_223277017422", "title": "[강쌤 문장구조] Ⅲ-8. <접속사 + 분사구문> 용법이 쓰이는 이유", "link": "https://blog.naver.com/kangreading/223277017422"});
+    blog.posts.push({"id": "post_223248106330", "title": "[강쌤 문장구조] Ⅰ.3. 관계부사절(where절, when절, why절, how절)이 명사절이 되는 이유", "link": "https://blog.naver.com/kangreading/223248106330"});
+    blog.posts.push({"id": "post_223275303672", "title": "[강쌤 문장구조] Ⅲ-7. 독립 분사구문이 쓰이는 이유", "link": "https://blog.naver.com/kangreading/223275303672"});
+    blog.posts.push({"id": "post_223281151165", "title": "[강쌤 문장구조] Ⅲ.11. <전치사 + 명사절>이 절 전체를 수식하는 용법", "link": "https://blog.naver.com/kangreading/223281151165"});
+    blog.posts.push({"id": "post_223259530434", "title": "[강쌤 문장구조] Ⅱ.5. 관계부사 where, when, why, how, that 용법", "link": "https://blog.naver.com/kangreading/223259530434"});
+    blog.posts.push({"id": "post_223265806431", "title": "[강쌤 문장구조] Ⅱ.11. ‘전치사 + 명사절’이 명사를 수식하는 용법", "link": "https://blog.naver.com/kangreading/223265806431"});
+    blog.posts.push({"id": "post_223280200665", "title": "[강쌤 문장구조] Ⅲ.10. 전치사구가 절 전체를 수식하는 부사구 용법", "link": "https://blog.naver.com/kangreading/223280200665"});
+    blog.posts.push({"id": "post_223270078559", "title": "[강쌤 문장구조] Ⅲ-1. 접속사와 관계부사가 이끄는 부사절의 구조", "link": "https://blog.naver.com/kangreading/223270078559"});
+    blog.posts.push({"id": "post_223266943934", "title": "[강쌤 문장구조] Ⅱ.12. 형용사가 뒤에서 명사를 수식하는 용법", "link": "https://blog.naver.com/kangreading/223266943934"});
+    blog.posts.push({"id": "post_223271058431", "title": "[강쌤 문장구조] Ⅲ-2. ‘so~that’과 ‘~so that’의 차이는 바로 ‘이것’", "link": "https://blog.naver.com/kangreading/223271058431"});
+    blog.posts.push({"id": "post_223263977083", "title": "[강쌤 문장구조] Ⅱ.9. 분사구가 문장 맨 앞에서 주어를 수식하는 용법", "link": "https://blog.naver.com/kangreading/223263977083"});
+    blog.posts.push({"id": "post_223268565147", "title": "[강쌤 문장구조] Ⅱ.13. 동격어구(완전 vs. 불완전; 제한적 vs 비제한적) 용법", "link": "https://blog.naver.com/kangreading/223268565147"});
+    blog.posts.push({"id": "post_223269239023", "title": "[강쌤 문장구조] Ⅲ. 부사류의 구조", "link": "https://blog.naver.com/kangreading/223269239023"});
+    blog.posts.push({"id": "post_223257255869", "title": "[강쌤 문장구조] Ⅱ.3. ‘전치사 + 관계대명사’, 제한 용법과 비제한 용법, 관계대명사와 선행사의 분리", "link": "https://blog.naver.com/kangreading/223257255869"});
+    blog.posts.push({"id": "post_223254423124", "title": "[강쌤 문장구조] Ⅱ.1. 관계대명사, 관계형용사, 관계부사 ... 쓰이는 이유, 공통점과 차이점", "link": "https://blog.naver.com/kangreading/223254423124"});
+    blog.posts.push({"id": "post_223255809892", "title": "[강쌤 문장구조] Ⅱ.2. 관계대명사와 선행사의 관계, who와 whom 뒤 어순, 관계대명사 뒤 삽입절", "link": "https://blog.naver.com/kangreading/223255809892"});
+    blog.posts.push({"id": "post_223258438195", "title": "[강쌤 문장구조] Ⅱ.4. 관계형용사 whose, which, what 용법", "link": "https://blog.naver.com/kangreading/223258438195"});
+    blog.posts.push({"id": "post_223260619564", "title": "[강쌤 문장구조] Ⅱ.6. 관계대명사와 관계부사, 생략 가능한 이유", "link": "https://blog.naver.com/kangreading/223260619564"});
+    blog.posts.push({"id": "post_223253428853", "title": "[강쌤 문장구조] Ⅱ. 형용사류의 구조", "link": "https://blog.naver.com/kangreading/223253428853"});
+    blog.posts.push({"id": "post_223251306977", "title": "[강쌤 문장구조] Ⅰ.5. 부정사구와 동명사구 ... 명사구로 쓰이는 이유", "link": "https://blog.naver.com/kangreading/223251306977"});
+    blog.posts.push({"id": "post_223262120538", "title": "[강쌤 문장구조] Ⅱ.8. 부정사구와 분사구 ... 형용사구로 쓰이는 이유", "link": "https://blog.naver.com/kangreading/223262120538"});
+    blog.posts.push({"id": "post_223261364628", "title": "[강쌤 문장구조] Ⅱ.7. that 용법(관계대명사, 접속사, 관계부사, 지시대명사, 지시형용사, 지시부사) 총정리", "link": "https://blog.naver.com/kangreading/223261364628"});
+    blog.posts.push({"id": "post_223245285061", "title": "[강쌤 문장구조] Ⅰ. 명사류의 구조", "link": "https://blog.naver.com/kangreading/223245285061"});
+    blog.posts.push({"id": "post_223248822318", "title": "[강쌤 문장구조] Ⅰ.4. 의문대명사절, 의문형용사절, 의문부사절이 명사절이 되는 이유", "link": "https://blog.naver.com/kangreading/223248822318"});
+    blog.posts.push({"id": "post_223249467049", "title": "[강쌤 문장구조] Ⅰ.4.1. 의문부사 how가 이끄는 절의 2가지 문장구조", "link": "https://blog.naver.com/kangreading/223249467049"});
+    blog.posts.push({"id": "post_223247895258", "title": "[강쌤 문장구조] I.1.1. 기출문제 “What is is right.”의 해석은?", "link": "https://blog.naver.com/kangreading/223247895258"});
+    blog.posts.push({"id": "post_223245991869", "title": "[강쌤 문장구조] Ⅰ.1. 명사절인 that절과 what절의 구조 차이", "link": "https://blog.naver.com/kangreading/223245991869"});
+    blog.posts.push({"id": "post_223244598375", "title": "[강쌤 문장구조] 왜 위치에 따라 영어 단어의 의미와 역할이 달라질까?", "link": "https://blog.naver.com/kangreading/223244598375"});
+    blog.posts.push({"id": "post_223247443718", "title": "[강쌤 문장구조] Ⅰ.2. 명사절인 whether절 / if절 구조", "link": "https://blog.naver.com/kangreading/223247443718"});
+    blog.posts.push({"id": "post_223242513135", "title": "[강쌤 문장구조] 5형식 구문의 대안, 명사류, 형용사류, 부사류로 분류하기", "link": "https://blog.naver.com/kangreading/223242513135"});
+    blog.posts.push({"id": "post_223243206359", "title": "[강쌤 문장구조]  영어 문장구조, 정해진 위치와 형태를 알면 쉽다.", "link": "https://blog.naver.com/kangreading/223243206359"});
+    blog.posts.push({"id": "post_223241627100", "title": "[강쌤 문장구조] 영어 5형식 구문, 왜 Reading에 걸림돌인가?", "link": "https://blog.naver.com/kangreading/223241627100"});
+    window.blogsData.push(blog);
+})();
