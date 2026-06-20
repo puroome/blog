745d1764f63fc9e98b4a79036475a402be3c7ea5
puroome <129413484+puroome@users.noreply.github.com>
2026-06-21 00:37:35 +0900

    Update _id_list.js

--- id/_id_list.js
+++ id/_id_list.js
@@ -1,1 +1,1 @@
-const activeBlogIds = ["design2kr", "design2kr_1", "leedovoca", "syette828", "yjk5863", "zealot01", "zeustour", "kangreading", "kangreading_1"];
+const activeBlogIds = ["design2kr", "design2kr_1", "leedovoca", "syette828", "yjk5863", "zealot01", "zeustour", "kangreading", "kangreading_1", "ersdf036", "ersdf036_1"];
