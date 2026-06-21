4ff88a65e89e9285f0546999c9193e80720e3df2
puroome <129413484+puroome@users.noreply.github.com>
2026-06-21 19:52:17 +0900

    Add files via upload

---
+++ id/eternity9us_4.js
@@ -0,0 +1,63 @@
+window.blogsData = window.blogsData || [];
+(function() {
+    let blog = {
+        "id": "eternity9us_7",
+        "name": "eternity9us_7",
+        "platform": "naver",
+        "blogUrl": "https://blog.naver.com/eternity9us",
+        "categoryNo": "43",
+        "parentCategoryNo": "",
+        "posts": []
+    };
+    blog.posts.push({"id": "post_222421893456", "title": "관계대명사(관계사) 제대로 이해하기 - 5. 관계대명사 what은 어떤 차이점이 있는가?", "link": "https://blog.naver.com/eternity9us/222421893456"});
+    blog.posts.push({"id": "post_222181617391", "title": "시간, 조건의 부사절에서는 현재가 미래를 대신한다. - 그런데 왜죠?", "link": "https://blog.naver.com/eternity9us/222181617391"});
+    blog.posts.push({"id": "post_222176367072", "title": "4형식 문장 구조에 대한 완전한 이해", "link": "https://blog.naver.com/eternity9us/222176367072"});
+    blog.posts.push({"id": "post_222174250878", "title": "시간은 at, 날짜는 on, 주, 달, 년도는 in, 어째서 서로 다를까?", "link": "https://blog.naver.com/eternity9us/222174250878"});
+    blog.posts.push({"id": "post_222168899858", "title": "현재완료는 왜 써야하고 어째서 \"have PP\"형태가 되죠?", "link": "https://blog.naver.com/eternity9us/222168899858"});
+    blog.posts.push({"id": "post_222154919721", "title": "한국어는 \"행복하게\" 영어는 \"행복한\" (feat. 5형식)", "link": "https://blog.naver.com/eternity9us/222154919721"});
+    blog.posts.push({"id": "post_222429800256", "title": "\"have to(~해야만 한다)\"의 부정은 어째서 \"~할 필요가 없다\"가 되는가?", "link": "https://blog.naver.com/eternity9us/222429800256"});
+    blog.posts.push({"id": "post_222674230145", "title": "the sleeping baby in the bed 표현과  the baby sleeping in the bed 표현의 이해", "link": "https://blog.naver.com/eternity9us/222674230145"});
+    blog.posts.push({"id": "post_222159258011", "title": "놀이터에 있는 \"시소\"가 영어라고? 그 보다 더 놀라운 사실도 있어요!", "link": "https://blog.naver.com/eternity9us/222159258011"});
+    blog.posts.push({"id": "post_221491868366", "title": "영어단어설명에 대해서...[필독]", "link": "https://blog.naver.com/eternity9us/221491868366"});
+    blog.posts.push({"id": "post_223451841752", "title": "교재(문법, 단어) 통합 안내입니다", "link": "https://blog.naver.com/eternity9us/223451841752"});
+    blog.posts.push({"id": "post_222406149431", "title": "관계대명사(관계사) 제대로 이해하기 - 1. 관계대명사는 왜 쓰는가?", "link": "https://blog.naver.com/eternity9us/222406149431"});
+    blog.posts.push({"id": "post_222408348341", "title": "관계대명사(관계사) 제대로 이해하기 - 2. 관계대명사는 어째서 관계대명사라고 부르는가?", "link": "https://blog.naver.com/eternity9us/222408348341"});
+    blog.posts.push({"id": "post_222183819616", "title": "4형식 문장의 3형식 전환에 대한 이해", "link": "https://blog.naver.com/eternity9us/222183819616"});
+    blog.posts.push({"id": "post_222191310724", "title": "4형식 문장의 3형식 전환. \"ask\"는 전치사 \"of\"를 쓴다.", "link": "https://blog.naver.com/eternity9us/222191310724"});
+    blog.posts.push({"id": "post_222416184740", "title": "관계대명사(관계사) 제대로 이해하기 - 4. 목적격 관계대명사는 왜 생략이 가능한가?", "link": "https://blog.naver.com/eternity9us/222416184740"});
+    blog.posts.push({"id": "post_222161382089", "title": "to부정사의 탄생 (feat. 5형식 문장의 구조)", "link": "https://blog.naver.com/eternity9us/222161382089"});
+    blog.posts.push({"id": "post_222413848246", "title": "관계대명사(관계사) 제대로 이해하기 - 3. 주격 관계대명사와 목적격 관계대명사는 무슨 차이인가?", "link": "https://blog.naver.com/eternity9us/222413848246"});
+    blog.posts.push({"id": "post_222148493526", "title": "자동사, 타동사를 구분하는 방법 (feat. see, look, hear, listen의 차이점)", "link": "https://blog.naver.com/eternity9us/222148493526"});
+    blog.posts.push({"id": "post_222152937349", "title": "전치사 \"for\"는 \"~를 위해서?\" 이렇게 공부하지 마세요.", "link": "https://blog.naver.com/eternity9us/222152937349"});
+    blog.posts.push({"id": "post_222146474876", "title": "영어에서 \"there is(are)~\" 표현은 도대체 왜 필요한 걸까?", "link": "https://blog.naver.com/eternity9us/222146474876"});
+    blog.posts.push({"id": "post_222482014186", "title": "가정법 제대로 이해하기 - 2) wish 가정법의 이해", "link": "https://blog.naver.com/eternity9us/222482014186"});
+    blog.posts.push({"id": "post_222455386588", "title": "not only A but also B표현과 A as well as B 표현의 이해 (1)", "link": "https://blog.naver.com/eternity9us/222455386588"});
+    blog.posts.push({"id": "post_222432287760", "title": "복합관계부사 \"wherever\"와 \"no matter where\"로 전환", "link": "https://blog.naver.com/eternity9us/222432287760"});
+    blog.posts.push({"id": "post_222493471411", "title": "가정법 제대로 이해하기 - 5) without 가정법, with 가정법", "link": "https://blog.naver.com/eternity9us/222493471411"});
+    blog.posts.push({"id": "post_222477471017", "title": "가정법 제대로 이해하기 - 1) 가정법의 기본구조, 가정법 미래, were to 가정법", "link": "https://blog.naver.com/eternity9us/222477471017"});
+    blog.posts.push({"id": "post_223451841752", "title": "교재(문법, 단어) 통합 안내입니다", "link": "https://blog.naver.com/eternity9us/223451841752"});
+    blog.posts.push({"id": "post_222466771380", "title": "not only A but also B 표현과 A as well as B 표현의 이해 (4)", "link": "https://blog.naver.com/eternity9us/222466771380"});
+    blog.posts.push({"id": "post_222501228285", "title": "가정법 제대로 이해하기 - 7) 혼합 가정법", "link": "https://blog.naver.com/eternity9us/222501228285"});
+    blog.posts.push({"id": "post_222457687255", "title": "not only A but also B표현과 A as well as B 표현의 이해 (2)", "link": "https://blog.naver.com/eternity9us/222457687255"});
+    blog.posts.push({"id": "post_222463967985", "title": "not only A but also B표현과 A as well as B 표현의 이해 (3)", "link": "https://blog.naver.com/eternity9us/222463967985"});
+    blog.posts.push({"id": "post_222473137449", "title": "not only A but also B 표현과 A as well as B 표현의 이해 (5)", "link": "https://blog.naver.com/eternity9us/222473137449"});
+    blog.posts.push({"id": "post_222497792239", "title": "가정법 제대로 이해하기 - 6) but for 가정법, if it were not for(if it had not been for) 가정법", "link": "https://blog.naver.com/eternity9us/222497792239"});
+    blog.posts.push({"id": "post_222400946222", "title": "5형식 문장의 예외적인 구조에 대한 이해", "link": "https://blog.naver.com/eternity9us/222400946222"});
+    blog.posts.push({"id": "post_222484176343", "title": "가정법 제대로 이해하기 - 3) as if 가정법과 as though 그리고 like 문장 표현", "link": "https://blog.naver.com/eternity9us/222484176343"});
+    blog.posts.push({"id": "post_222425444385", "title": "복합관계부사 \"whenever\"와 \"no matter when\"으로의 전환", "link": "https://blog.naver.com/eternity9us/222425444385"});
+    blog.posts.push({"id": "post_221491868366", "title": "영어단어설명에 대해서...[필독]", "link": "https://blog.naver.com/eternity9us/221491868366"});
+    blog.posts.push({"id": "post_222489923323", "title": "가정법 제대로 이해하기 - 4) would rather 가정법 그리고 had better 표현과의 구분", "link": "https://blog.naver.com/eternity9us/222489923323"});
+    blog.posts.push({"id": "post_222438178367", "title": "It ~ that 강조구문(용법)의 이해와 활용, It ~ that 강조구문을 사용하는 이유", "link": "https://blog.naver.com/eternity9us/222438178367"});
+    blog.posts.push({"id": "post_222392244655", "title": "시제 일치 그리고 시제 일치의 예외에 대한 이해 - 4) 시간과 조건의 부사절에서는 현재가 미래를 대신한다.", "link": "https://blog.naver.com/eternity9us/222392244655"});
+    blog.posts.push({"id": "post_222389445328", "title": "시제 일치 그리고 시제 일치의 예외에 대한 이해 - 3) 시제 일치가 적용되지 않는 예외적인 경우", "link": "https://blog.naver.com/eternity9us/222389445328"});
+    blog.posts.push({"id": "post_222369529091", "title": "\"~때까지\" 뭘 써야 하는 거죠? until(till)과 by의 차이점", "link": "https://blog.naver.com/eternity9us/222369529091"});
+    blog.posts.push({"id": "post_222382401212", "title": "시제 일치 그리고 시제 일치의 예외에 대한 이해 - 2) 과거완료와 대과거의 이해", "link": "https://blog.naver.com/eternity9us/222382401212"});
+    blog.posts.push({"id": "post_222378534834", "title": "시제 일치 그리고 시제 일치의 예외에 대한 이해 - 1) 기본 개념", "link": "https://blog.naver.com/eternity9us/222378534834"});
+    blog.posts.push({"id": "post_222349080315", "title": "관계대명사의 계속적 용법과 한정적 용법 - 도대체 뭐고, 무슨 차이가 있는 거죠?", "link": "https://blog.naver.com/eternity9us/222349080315"});
+    blog.posts.push({"id": "post_222346209902", "title": "혼동되는 kind of, a kind of, kinds of 표현에 대한 이해 - 영어혁명 문법완성", "link": "https://blog.naver.com/eternity9us/222346209902"});
+    blog.posts.push({"id": "post_222398764194", "title": "5형식 문장의 예외적인 구조에 대한 이해", "link": "https://blog.naver.com/eternity9us/222398764194"});
+    blog.posts.push({"id": "post_222358286305", "title": "관계대명사의 계속적 용법과 한정적 용법 - 계속적 용법의 구조 전환", "link": "https://blog.naver.com/eternity9us/222358286305"});
+    blog.posts.push({"id": "post_222355616145", "title": "관계대명사의 계속적 용법과 한정적 용법 - \"that\"은 계속적 용법으로 쓸 수 없다.", "link": "https://blog.naver.com/eternity9us/222355616145"});
+    blog.posts.push({"id": "post_222365848705", "title": "some은 긍정문 any는 부정문? some과 any의 차이점 - 정확히 이해하기", "link": "https://blog.naver.com/eternity9us/222365848705"});
+    window.blogsData.push(blog);
+})();
