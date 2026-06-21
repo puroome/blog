d99bc3e3e633f0062b96bdd70d3f577e7b23b707
puroome <129413484+puroome@users.noreply.github.com>
2026-06-21 19:59:44 +0900

    Update eternity9us_3.js

--- id/eternity9us_3.js
+++ id/eternity9us_3.js
@@ -1,8 +1,8 @@
 window.blogsData = window.blogsData || [];
 (function() {
     let blog = {
-        "id": "eternity9us_6",
-        "name": "eternity9us_6",
+        "id": "eternity9us_3",
+        "name": "eternity9us_3",
         "platform": "naver",
         "blogUrl": "https://blog.naver.com/eternity9us",
         "categoryNo": "32",
